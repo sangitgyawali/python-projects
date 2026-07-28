fd9931b412feff929514de077f888347e7ce8d83
Sangit Gyawali <developersangeet@gmail.com>
2026-07-28 10:41:19 +0545

    Initial commit

---
+++ Calculator/main.py
@@ -0,0 +1,49 @@
+# Calculator project using Python
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
+
+
+while True:
+    print("\n===== PYTHON CALCULATOR =====")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == "5":
+        print("Thank you for using the calculator!")
+        break
+
+    if choice in ["1", "2", "3", "4"]:
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == "1":
+                print("Result:", add(num1, num2))
+            elif choice == "2":
+                print("Result:", sub(num1, num2))
+            elif choice == "3":
+                print("Result:", multiply(num1, num2))
+            elif choice == "4":
+                print("Result:", divide(num1, num2))
+
+        except ValueError:
+            print("Invalid input! Please enter numbers only.")
+    else:
+        print("Invalid choice! Please try again.")

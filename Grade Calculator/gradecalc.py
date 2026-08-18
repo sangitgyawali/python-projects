e589ea04c08c961ed45ed6c1a71b5edcc210bcd8
Sangit Gyawali <developersangeet@gmail.com>
2026-08-18 22:18:38 +0545

    Grade Calculator

---
+++ Grade Calculator/gradecalc.py
@@ -0,0 +1,14 @@
+marks = float(input("Enter marks: "))
+
+if marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Grade:", grade)

9f5f7228d681b084ba35ae459f72c74171ef10e7
Sangit Gyawali <developersangeet@gmail.com>
2026-08-26 16:32:58 +0545

    Expense Tracker advanced add expense function

---
+++ Expense Tracker Advanced/expensetracker.py
@@ -0,0 +1,17 @@
+expenses = []
+
+def add_expense():
+    name = input("What did you buy? ")
+    amount = float(input("How much did it costs?"))
+    category = input("Category:")
+
+    expense = {
+        "name" = name
+        "amount" = amount
+        "catgory" = category
+    }
+
+    expenses.append(expenses)
+
+    print("Expense added successfully!")
+    

92ee22677d402b421764c5a88dcf525d9833378e
Sangit Gyawali <developersangeet@gmail.com>
2026-08-26 16:38:52 +0545

    Expense Tracker advanced show expense function

--- Expense Tracker Advanced/expensetracker.py
+++ Expense Tracker Advanced/expensetracker.py
@@ -14,4 +14,18 @@ def add_expense():
     expenses.append(expenses)
 
     print("Expense added successfully!")
-    
+
+def show_expenses():
+    if len(expenses) == 0:
+        print("No expenses found.")
+        return
+
+    print("\n--- All Expenses ---")
+
+    for expense in expenses:
+        print(
+            f"{expense['name']} | "
+            f"Rs. {expense['amount']} | "
+            f"{expense['category']}"
+        )
+        )
